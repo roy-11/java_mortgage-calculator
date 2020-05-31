e40b06601d9188a151e81b8492a99aeb261f3be6
ryosukehagiwara <hukurahagi1109@gmail.com>
2020-05-31 19:45:36 +0900

    get user input data

--- src/com/practice/Main.java
+++ src/com/practice/Main.java
@@ -1,8 +1,26 @@
 package com.practice;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("ローンの金額: ");
+        int principal = scanner.nextInt();
+
+        System.out.println("年利: ");
+        float annualInterest = scanner.nextFloat();
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+
+        System.out.println("期間(年数): ");
+        byte years = scanner.nextByte();
+        int numberOfPayments = years * MONTHS_IN_YEAR;
+
+
     }
 }
